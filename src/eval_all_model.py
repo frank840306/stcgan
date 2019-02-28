b6005c6af0c12ebffd55ed68b13ee41afbef55d3
frank840306 <frank840306@gmail.com>
2019-02-28 16:12:58 +0800

    update eval_all_model postfix

--- src/eval_all_model.py
+++ src/eval_all_model.py
@@ -12,6 +12,7 @@ def get_args():
     parser.add_argument('--batch_size_test', default=4, type=int, help='batch size of testing image')
     parser.add_argument('--lrG', default=0.001, type=float, help='lr of G')
     parser.add_argument('--lrD', default=0.001, type=float, help='lr of D')
+    parser.add_argument('--postfix', default='', type=str, help='postfix')
     parser.add_argument('--dataset_name', default='ISTD', type=str, help='ISTD or DSRD')
     return parser.parse_args()
 
@@ -21,7 +22,7 @@ def eval_model(eval_type='best'):
     best_models = sorted(list(set([bm.split('/')[-1][:-8] for bm in best_models])), reverse=True)
     for m in best_models:
         # pass
-        os.system('./run.sh --load_model --gpu_id 0 --model_name {} --lrG {} --lrD {} --batch_size_test {} --root_dir {} --dataset_name {}'.format(m, args.lrG, args.lrD, args.batch_size_test, args.root_dir, args.dataset_name))
+        os.system('./run.sh --load_model --gpu_id 0 --model_name {} --lrG {} --lrD {} --batch_size_test {} --root_dir {} --dataset_name {} --postfix {}'.format(m, args.lrG, args.lrD, args.batch_size_test, args.root_dir, args.dataset_name, args.postfix))
 
 def eval_all():
     eval_model('best')
@@ -29,7 +30,7 @@ def eval_all():
     
 if __name__ == "__main__":
     args = get_args()
-    path = PathHandler(args.root_dir, task='stcgan_lrG_{}_lrD_{}'.format(args.lrG, args.lrD), data=args.dataset_name)
+    path = PathHandler(args.root_dir, task='stcgan_lrG_{}_lrD_{}{}'.format(args.lrG, args.lrD, args.postfix), data=args.dataset_name)
     
     log_file = os.path.join(path.log_dir, os.path.basename(__file__) + '.log')
     set_logger(log_file)
